c97c8aa273144f6267ead9d621b55acde695fe41
1899051 <rajlokesh117@gmail.com>
2023-01-04 18:48:31 +0530

    initial commit

---
+++ src/main/java/com/restclient/NYTimesRestClient/NyTimesRestClientApplication.java
@@ -0,0 +1,13 @@
+package com.restclient.NYTimesRestClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyTimesRestClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyTimesRestClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restclient/NYTimesRestClient/NyTimesRestClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restclient.NYTimesRestClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyTimesRestClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
